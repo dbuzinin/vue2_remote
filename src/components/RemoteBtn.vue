69bb7481c2bd3dbf345b3a991cb5021d2deb605c
Dmitriy Buzinin <dbuzinin@voximplant.com>
2023-10-20 16:34:27 +0300

    feat Рабочий вариант (dynamic импорт)

--- src/components/RemoteBtn.vue
+++ src/components/RemoteBtn.vue
@@ -4,6 +4,7 @@ import { Vue, Component, Prop } from 'vue-property-decorator';
 @Component({})
 export default class RemoteBtn extends Vue {
   @Prop({default: false}) item!: boolean;
+  @Prop({default: 'My remote btn'}) value!: string;
   mounted(): void {
     console.log('Remote_btn', this)
   }
@@ -11,13 +12,16 @@ export default class RemoteBtn extends Vue {
 </script>
 
 <template>
-<div class="btn" @click="$emit('load')">My btn {{item}}</div>
+<div class="btn" @click="$emit('click')">{{value}}</div>
 </template>
 
 <style scoped>
 .btn {
+  display: inline-flex;
   padding: 8px;
   background-color: #42b983;
   color: white;
+  cursor: pointer;
+  border-radius: 8px;
 }
 </style>
